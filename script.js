0607cd213bbd081b1e05e3895c97da6c1e5ffab9
fudge21 <113610178+fudge21@users.noreply.github.com>
2024-02-15 22:52:14 +0000

    major updates to website ui and more info

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Notification.requestPermission().then(perm => {
+    if (perm === "granted") {
+        const notification = new Notification("Test", {
+            body: "Hello",
+        })
+
+        notification.addEventListener("click", e => {
+            window.open("https://google.com")
+        })
+    }
+})
+*/
+
+

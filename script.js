e8f91b0f9dee9abb6f2555df6b508a224f04661b
fudge21 <113610178+fudge21@users.noreply.github.com>
2024-04-29 22:42:32 +0000

    Major UX Improvements

--- script.js
+++ script.js
@@ -12,4 +12,11 @@ Notification.requestPermission().then(perm => {
 })
 */
 
-
+var hamburgerButtons = document.querySelector("#hamburgerButtons")
+document.querySelector("#hamburger").addEventListener("click", function () {
+    if (hamburgerButtons.style.display == "none" || hamburgerButtons.style.display == "") {
+        hamburgerButtons.style.display = "block"
+    } else {
+        hamburgerButtons.style.display = "none"
+    }
+})

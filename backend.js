27c0d22db08cef9ac0aedfc068d041254d8278f4
fudge21 <113610178+fudge21@users.noreply.github.com>
2024-11-22 02:06:47 +0000

    redirect fix test

--- backend.js
+++ backend.js
@@ -100,7 +100,7 @@ if (window.location.pathname == "/auth/") {
   });
 } else {
   onAuthStateChanged(auth, (user) => {
-    if (user && user.displayName == null) {
+    if (user && user.displayName == null || user.displayName == "") {
       window.location.href = "/auth/"
     }
   });
